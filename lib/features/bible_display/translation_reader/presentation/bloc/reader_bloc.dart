9412c6a5f5a116c6287b9d0db02a2c2f30dad4e2
jerome <jerome.bandril@gmail.com>
2024-08-17 21:37:43 +0200

    refactor reader bloc and state

--- lib/features/bible_display/translation_reader/presentation/bloc/reader_bloc.dart
+++ lib/features/bible_display/translation_reader/presentation/bloc/reader_bloc.dart
@@ -1,28 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:the_smyrna_bible_v2/core/utils/bible_reference_parser.dart';
-import 'package:the_smyrna_bible_v2/features/bible_display/scripture_finder/domain/entity/bible_reference.dart';
-import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/close_usfx_translation.dart';
+import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/get_translation.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/read_chapter.dart';
-import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/load_usfx_translation.dart';
+import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/open_usfx_translation.dart';
+
+import '../../../../translations_installer_manager/domain/entities/translation.dart';
+import '../../../searchbar/domain/entity/bible_reference.dart';
 
 part 'reader_event.dart';
 part 'reader_state.dart';
 
 class ReaderBloc extends Bloc<ReaderEvent, ReaderState> {
-  final ReadUsfxTranslation readTranslation;
-  final CloseUsfxTranslation closeTranslation;
+  final OpenUsfxTranslation openTranslation;
+  // final CloseUsfxTranslation closeTranslation;
   final ReadChapter readChapter;
   final BibleReferenceParser parser;
+  final GetTranslation getTranslation;
 
   ReaderBloc({
-    required this.readTranslation,
-    required this.closeTranslation,
+    required this.openTranslation,
+    // required this.closeTranslation,
     required this.readChapter,
     required this.parser,
+    required this.getTranslation,
   }) : super(const ReaderState()) {
     on<ReaderLoadTranslation>(_onLoadTranslation);
-    on<ReaderAnalyzePrompt>(_onAnalyzePrompt);
+    on<ReaderSetContent>(_onSetContent);
   }
 
   Future<void> _onLoadTranslation(
@@ -31,40 +35,39 @@ class ReaderBloc extends Bloc<ReaderEvent, ReaderState> {
   ) async {
     emit(state.copyWith(status: () => ReaderStatus.reading));
 
-    final eitherFailureOrTranslation = await readTranslation(event.id);
+    final eitherFailureOrTranslation = await openTranslation(event.id);
     eitherFailureOrTranslation.fold(
-      (failure) => emit(state.copyWith(status: () => ReaderStatus.error)),
+      (failure) => print(
+        "> BReader: error while loading translation",
+      ), //emit(state.copyWith(status: () => ReaderStatus.error)),
       (translation) {
         return emit(
           state.copyWith(
             status: () => ReaderStatus.success,
+            TEMP_TRANSLATION: () => translation,
           ),
         );
       },
     );
   }
 
-  Future<void> _onAnalyzePrompt(
-    ReaderAnalyzePrompt event,
+  Future<void> _onSetContent(
+    ReaderSetContent event,
     Emitter<ReaderState> emit,
   ) async {
-    final eitherFailureOrReference = parser.analyze(event.text);
+    // final eitherFailureOrReference = parser.analyze(event.text);
 
-    return eitherFailureOrReference.fold(
-      (l) => emit(state.copyWith(status: () => ReaderStatus.error)),
-      (reference) async {
-        final eitherFailureOrChapter = await readChapter(reference);
-        return eitherFailureOrChapter.fold(
-          (_) => print('error'),
-          (chapter) => emit(
-            state.copyWith(
-              status: () => ReaderStatus.success,
-              reference: () => reference,
-              references: () => [chapter],
-            ),
-          ),
-        );
-      },
+    // return eitherFailureOrReference.fold(
+    final eitherFailureOrChapter = await readChapter(event.ref);
+    return eitherFailureOrChapter.fold(
+      (_) => print('> BReader: error'),
+      (verses) => emit(
+        state.copyWith(
+          status: () => ReaderStatus.success,
+          reference: () => event.ref,
+          references: () => verses,
+        ),
+      ),
     );
   }
 }
--- lib/features/bible_display/translation_reader/presentation/bloc/reader_state.dart
+++ lib/features/bible_display/translation_reader/presentation/bloc/reader_state.dart
@@ -10,23 +10,28 @@ enum ReaderStatus {
 class ReaderState extends Equatable {
   const ReaderState({
     this.status = ReaderStatus.initial,
-    this.reference = const BibleReference(book: '', chapter: 0, verse: 0),
+    this.reference = const BibleReference(),
     this.references = const [],
+    this.TEMP_TRANSLATION,
   });
 
   final ReaderStatus status;
   final BibleReference reference;
   final List<BibleReference> references;
+  final Translation? TEMP_TRANSLATION;
 
   ReaderState copyWith({
     ReaderStatus Function()? status,
     BibleReference Function()? reference,
     List<BibleReference> Function()? references,
+    Translation Function()? TEMP_TRANSLATION,
   }) {
     return ReaderState(
       status: status != null ? status() : this.status,
       reference: reference != null ? reference() : this.reference,
       references: references != null ? references() : this.references,
+      TEMP_TRANSLATION:
+          TEMP_TRANSLATION != null ? TEMP_TRANSLATION() : this.TEMP_TRANSLATION,
     );
   }
 
