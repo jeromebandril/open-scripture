2aa3aa253d985401834d3fe38f86a369abf78904
jerome <jerome.bandril@gmail.com>
2024-08-17 21:36:47 +0200

    ADD: get tranlsation usecase

--- lib/features/bible_display/translation_reader/data/repositories/reader_repository_impl.dart
+++ lib/features/bible_display/translation_reader/data/repositories/reader_repository_impl.dart
@@ -3,24 +3,30 @@ import 'package:the_smyrna_bible_v2/core/error/exception.dart';
 import 'package:the_smyrna_bible_v2/core/error/failure.dart';
 import 'package:the_smyrna_bible_v2/core/models/translation_model.dart';
 import 'package:the_smyrna_bible_v2/core/utils/bible_reference_parser.dart';
-import 'package:the_smyrna_bible_v2/features/bible_display/scripture_finder/domain/entity/bible_reference.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/data/datasources/translations_datasource.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/repositories/reader_repository.dart';
+import 'package:the_smyrna_bible_v2/features/translations_installer_manager/domain/entities/translation.dart';
+import 'package:the_smyrna_bible_v2/features/translations_installer_manager/presentation/bloc/translation_download_progress/translation_download_progress_bloc.dart';
+
+import '../../../searchbar/domain/entity/bible_reference.dart';
 
 class ReaderRepositoryImpl implements ReaderRepository {
-  final TranslationsDataSource dataSource;
+  Map<String, TranslationModel> translationPool = {};
+  final TranslationsDataSource localDataSource;
 
   ReaderRepositoryImpl({
-    required this.dataSource,
+    required this.localDataSource,
   });
 
   @override
-  Future<Either<Failure, TranslationModel>> getTranslation(String id) async {
+  Future<Either<Failure, TranslationModel>> openTranslation(String id) async {
     try {
-      if (translationPool.pool[id] == null) {
-        translationPool.pool[id] = await dataSource.getTranslation(id);
+      if (translationPool[id] == null) {
+        print("> BReader: load for the first time $id");
+        translationPool[id] = await localDataSource.getTranslation(id);
       }
-      return Right(translationPool.pool[id]! as TranslationModel);
+      print("> BReader: translation loaded successfully");
+      return Right(translationPool[id]!);
     } on NoLocalDataException {
       return const Left(NoLocalDataFailure());
     }
@@ -29,7 +35,7 @@ class ReaderRepositoryImpl implements ReaderRepository {
   @override
   Future<Either<Failure, void>> removeTranslation(String id) async {
     try {
-      reader.translations.removeWhere((t) => t.abbreviation == id);
+      // reader.translations.removeWhere((t) => t.abbreviation == id);
       return const Right(null);
 
       // if all views are don't use a translation
@@ -40,9 +46,18 @@ class ReaderRepositoryImpl implements ReaderRepository {
   }
 
   @override
-  Future<Either<Failure, BibleReference>> getChapter(BibleReference ref) async {
+  Future<Either<Failure, List<BibleReference>>> getChapter(
+      BibleReference ref) async {
     try {
-      final book = reader.translations.first.bookNames.values
+      //
+      // Use temporarly eng-kjv for test which is the most common
+      // TODO: replace in the future with a variable or parameter
+      //
+      // Find the given reference in the given book
+      //
+      final book = translationPool['eng-kjv']!
+          .bookNames
+          .values
           .where(
             (bookData) =>
                 bookData.abbr.toUpperCase().contains(ref.book) ||
@@ -50,20 +65,48 @@ class ReaderRepositoryImpl implements ReaderRepository {
                 bookData.short.toUpperCase().contains(ref.book),
           )
           .firstOrNull;
-      print(book);
-      if (book == null) throw InvalidInputException();
-      if (ref.chapter > book.chapters.length || ref.chapter < 0) {
+      //
+      // Handle exceptions
+      //
+      if (book == null ||
+          ref.chapter > book.chapters.length ||
+          ref.chapter < 0) {
         throw InvalidInputException();
       }
+      //
+      // Make a list of all References of the chapter
+      // in which the given reference is
+      // (in this case it corrispond to each verse of the same chapter)
+      //
       List<BibleReference> temp = [];
+      for (int i = 0;
+          // iterate trough the verses
+          // (which are grouped by paragraphs in this case)
+          i <
+              translationPool['eng-kjv']!
+                  .bookNames[book.abbr.toUpperCase()]!
+                  .chapters[ref.chapter]
+                  .paragraphs
+                  .map((paragraph) => paragraph.verses.length)
+                  .fold(0, (prev, count) => prev + count);
+          i++) {
+        temp.add(
+            BibleReference(book: ref.book, chapter: ref.chapter, verse: i));
+      }
 
-      // for (int i = 0; i < bible[book][chapter - 1].length; i++) {
-      //   temp.add(BibleReference(book: book, chapter: chapter - 1, verse: i));
-      // }
-      // create List of reference from verse one to last verse of the chapter
-      return Right(ref);
+      print("> Reader: result is ${temp.length} verses");
+      return Right(temp);
     } on InvalidInputException {
       return Left(InvalidInputFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, Translation>> getTranslation(String id) {
+    if (translationPool[id] != null) {
+      return Future.value(Right(translationPool[id]!));
+    } else {
+      return Future.value(const Left(NoLocalDataFailure()));
     }
   }
 }
--- lib/features/bible_display/translation_reader/domain/repositories/reader_repository.dart
+++ lib/features/bible_display/translation_reader/domain/repositories/reader_repository.dart
@@ -1,11 +1,13 @@
 import 'package:fpdart/fpdart.dart';
-import 'package:the_smyrna_bible_v2/features/bible_display/scripture_finder/domain/entity/bible_reference.dart';
+import 'package:the_smyrna_bible_v2/features/bible_display/searchbar/domain/entity/bible_reference.dart';
 
 import '../../../../../core/error/failure.dart';
 import '../../../../translations_installer_manager/domain/entities/translation.dart';
 
 abstract class ReaderRepository {
+  Future<Either<Failure, Translation>> openTranslation(String id);
+  // Future<Either<Failure, void>> removeTranslation(String id);
+  Future<Either<Failure, List<BibleReference>>> getChapter(
+      BibleReference reference);
   Future<Either<Failure, Translation>> getTranslation(String id);
-  Future<Either<Failure, void>> removeTranslation(String id);
-  Future<Either<Failure, BibleReference>> getChapter(BibleReference reference);
 }
