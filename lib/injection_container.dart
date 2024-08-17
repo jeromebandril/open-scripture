6f8150abeb6f669f5d255322f199eadea34104cc
jerome <jerome.bandril@gmail.com>
2024-08-17 21:39:49 +0200

    add missing depedency

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,16 +1,18 @@
 import 'package:get_it/get_it.dart';
 import 'package:the_smyrna_bible_v2/core/models/translation_manager_model.dart';
 import 'package:the_smyrna_bible_v2/core/utils/bible_reference_parser.dart';
+import 'package:the_smyrna_bible_v2/features/bible_display/split_screen/domain/usecases/split_horizontally.dart';
+import 'package:the_smyrna_bible_v2/features/bible_display/split_screen/domain/usecases/split_vertically.dart';
+import 'package:the_smyrna_bible_v2/features/bible_display/split_screen/presenter/bloc/split_screen_bloc.dart';
+import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/get_translation.dart';
 import 'package:the_smyrna_bible_v2/features/settings_window/presentation/bloc/settings_bloc.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/data/datasources/translations_datasource.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/data/repositories/reader_repository_impl.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/repositories/reader_repository.dart';
-import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/close_usfx_translation.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/read_chapter.dart';
-import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/load_usfx_translation.dart';
-import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/presentation/bloc/reader_bloc.dart';
+import 'package:the_smyrna_bible_v2/features/bible_display/translation_reader/domain/usecases/open_usfx_translation.dart';
 import 'package:the_smyrna_bible_v2/features/translations_installer_manager/data/datasources/translation_manager_remote_datasource.dart';
-import 'package:the_smyrna_bible_v2/features/translations_installer_manager/data/datasources/translation_manager_local_datasource.dart';
+import 'package:the_smyrna_bible_v2/core/datasources/translation_manager_local_datasource.dart';
 import 'package:the_smyrna_bible_v2/features/translations_installer_manager/data/repositories/translation_manager_repository_impl.dart';
 import 'package:the_smyrna_bible_v2/features/translations_installer_manager/domain/repositories/translation_manager_repository.dart';
 import 'package:the_smyrna_bible_v2/features/translations_installer_manager/domain/usecases/get_local_translations_info_list.dart';
@@ -20,6 +22,8 @@ import 'package:the_smyrna_bible_v2/features/translations_installer_manager/pres
 import 'package:the_smyrna_bible_v2/features/translations_installer_manager/presentation/bloc/translation_download_progress/translation_download_progress_bloc.dart';
 import 'package:the_smyrna_bible_v2/features/translations_installer_manager/presentation/bloc/translation_manager_bloc.dart';
 import 'package:the_smyrna_bible_v2/features/window_stack_manager/presentation/bloc/window_stack_manager_bloc.dart';
+import 'features/bible_display/searchbar/presenter/bloc/b_searchbar_bloc.dart';
+import 'features/bible_display/translation_reader/presentation/bloc/reader_bloc.dart';
 import 'features/translations_installer_manager/domain/usecases/download_translation.dart';
 import 'features/translations_installer_manager/domain/usecases/install_translation.dart';
 import 'features/translations_installer_manager/presentation/bloc/translations_overview/translations_bloc.dart';
@@ -91,25 +95,44 @@ Future<void> init() async {
   // bloc
   sl.registerFactory(
     () => ReaderBloc(
-      readTranslation: sl(),
-      closeTranslation: sl(),
+      // closeTranslation: sl(),
       readChapter: sl(),
       parser: sl(),
+      openTranslation: sl(),
+      getTranslation: sl(),
     ),
   );
 
-  sl.registerLazySingleton(() => ReadUsfxTranslation(sl()));
-  sl.registerLazySingleton(() => CloseUsfxTranslation(sl()));
+  sl.registerLazySingleton(() => OpenUsfxTranslation(sl()));
+  // sl.registerLazySingleton(() => CloseUsfxTranslation(sl()));
+
   sl.registerLazySingleton(() => ReadChapter(sl()));
   sl.registerLazySingleton(() => BibleReferenceParser());
+  sl.registerLazySingleton(() => GetTranslation(sl()));
 
   sl.registerLazySingleton<ReaderRepository>(
     () => ReaderRepositoryImpl(
-      dataSource: sl(),
+      localDataSource: sl(),
     ),
   );
 
   sl.registerLazySingleton<TranslationsDataSource>(
     () => TranslationsDataSourceImpl(),
+  );
+
+  // Features - Bible reader
+  // bloc
+  sl.registerFactory(
+    () => SplitScreenBloc(
+      splitX: sl(),
+      splitY: sl(),
+    ),
+  );
+  sl.registerLazySingleton<SplitHorizontally>(() => SplitHorizontally());
+  sl.registerLazySingleton<SplitVertically>(() => SplitVertically());
+
+  // Features - BSearchbar
+  sl.registerFactory(
+    () => BSearchbarBloc(brParser: sl()),
   );
 }
