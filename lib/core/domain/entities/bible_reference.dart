6df77cdbf2616fa62935ecf8e2141e7e69b1046a
jerome <jerome.bandril@gmail.com>
2024-09-02 13:45:25 +0200

    renamed class to BibleRef

--- lib/core/domain/entities/bible_reference.dart
+++ lib/core/domain/entities/bible_reference.dart
@@ -1,11 +1,11 @@
 import 'package:equatable/equatable.dart';
 
-class BibleReference extends Equatable {
+class BibleRef extends Equatable {
   final String book;
   final int chapter;
   final int verse;
 
-  const BibleReference({
+  const BibleRef({
     this.book = '',
     this.chapter = 0,
     this.verse = 0,
