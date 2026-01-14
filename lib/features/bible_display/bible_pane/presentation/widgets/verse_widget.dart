1d37dc8d797e572344b1ca996cfa08eb32ec9518
jerome <jerome.bandril@gmail.com>
2026-01-14 15:47:52 +0100

    improve ui

--- lib/features/bible_display/bible_pane/presentation/widgets/verse_widget.dart
+++ lib/features/bible_display/bible_pane/presentation/widgets/verse_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/bible_pane/presentation/bloc/bible_pane_bloc.dart';
 import 'package:the_smyrna_bible_v2/features/bible_display/bible_pane/presentation/cubit/selected_word_cubit.dart';
 
+import '../../../../../core/domain/entities/bible_ref.dart';
 import '../../../../../core/domain/entities/verse_segment.dart';
 import '../../../../../core/domain/entities/verse_span.dart';
 import '../rendering/verse_richtext_builder.dart';
@@ -24,37 +25,44 @@ class VerseWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String content = segments.map((e) => e.textContent).join();
+    final BibleRef ref = segments.first.ref;
 
-    return Listener(
-      onPointerDown: (_) {
-        final ref = context.read<BiblePaneBloc>().state.reference!;
-        context.read<BiblePaneBloc>().add(BiblePaneJustChangeRef(
-                ref: ref.copyWith(
-              verseStart: verseNumber,
-              verseEnd: null,
-            )));
-      },
-      child: spans == null
-          ? Text(content)
-          : SelectableText.rich(TextSpan(children: [
-              TextSpan(
-                text: '$verseNumber  ',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: isHighlighted
-                        ? Colors.cyan
-                        : Theme.of(context).colorScheme.onSurface),
-              ),
-              VerseSpanBuilder.build(
-                text: content,
-                spans: spans!,
-                onWordTap: (VerseSpan span, String slice) =>
-                    context.read<SelectedWordCubit>().setSelectedWord(WordInfo(
-                          text: slice,
-                          span: span,
-                        )),
-              ),
-            ])),
+    return Padding(
+      padding: EdgeInsets.only(bottom: 36),
+      child: Listener(
+        onPointerDown: (_) {
+          final ref = context.read<BiblePaneBloc>().state.reference!;
+          context.read<BiblePaneBloc>().add(BiblePaneJustChangeRef(
+                  ref: ref.copyWith(
+                verseStart: verseNumber,
+                verseEnd: null,
+              )));
+        },
+        child: spans == null
+            ? Text(content)
+            : SelectableText.rich(
+                TextSpan(style: TextStyle(height: 1.2), children: [
+                TextSpan(
+                  text:
+                      isHighlighted ? '${ref.toString()}  ' : '$verseNumber  ',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: isHighlighted
+                          ? Colors.blueAccent
+                          : Theme.of(context).colorScheme.onSurface),
+                ),
+                VerseSpanBuilder.build(
+                  text: content,
+                  spans: spans!,
+                  onWordTap: (VerseSpan span, String slice) => context
+                      .read<SelectedWordCubit>()
+                      .setSelectedWord(WordInfo(
+                        text: slice,
+                        span: span,
+                      )),
+                ),
+              ])),
+      ),
     );
   }
 }
