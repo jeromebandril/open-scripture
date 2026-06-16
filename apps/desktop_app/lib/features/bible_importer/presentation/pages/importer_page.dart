75711d764e5666b5e4a7ea0ae7dbbd3ab84aa61f
jerome <jerome.bandril@gmail.com>
2026-06-16 12:48:57 +0200

    feat: add sword modules download links

--- apps/desktop_app/lib/features/bible_importer/presentation/pages/importer_page.dart
+++ apps/desktop_app/lib/features/bible_importer/presentation/pages/importer_page.dart
@@ -3,17 +3,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:open_scripture/core/di/injection_container.dart';
 import 'package:open_scripture/features/bible_importer/presentation/state/bible_importer_cubit/bible_importer_cubit.dart';
 import 'package:open_scripture/features/bible_importer/presentation/widgets/importer.dart';
+import 'package:open_scripture/features/bible_importer/presentation/widgets/list_of_repositories.dart';
 import 'package:open_scripture/features/bible_importer/presentation/widgets/sword_path_selector.dart';
 import 'package:open_scripture/features/settings_window/presentation/widgets/setting.dart';
 import 'package:open_scripture/features/settings_window/presentation/widgets/setting_input_option.dart';
 import 'package:open_scripture/features/settings_window/presentation/widgets/setting_section.dart';
 import 'package:open_scripture/shared/enums/bible_repository_type.dart';
-import 'package:open_scripture/shared/theme/tokens.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 const _recommendedSources = [
   'https://eBible.org',
   'https://github.com/seven1m/open-bibles'
+];
+
+const _crosswireSources = [
+  "https://www.crosswire.org/sword/modules/ModDisp.jsp?modType=Bibles",
+  "https://ftp.crosswire.org/ftpmirror/pub/sword/raw/modules/texts/ztext/",
 ];
 
 class ImporterPage extends StatelessWidget {
@@ -55,58 +59,20 @@ class ImporterPage extends StatelessWidget {
                 ],
               ),
               SettingSection(title: 'Recommended repositories', children: [
-                const _ListOfBibleRepositories(),
-                const _ListOfBibleRepositories(),
+                const ListOfBibleRepositories(
+                  urls: _recommendedSources,
+                  description: 'Repositories for canonical installations.',
+                ),
+                const ListOfBibleRepositories(
+                  urls: _crosswireSources,
+                  description:
+                      'Repositories for crosswire sword engine. The first link may not work.',
+                ),
               ]),
             ],
           ),
         );
       }),
     );
-  }
-}
-
-class _ListOfBibleRepositories extends StatelessWidget {
-  const _ListOfBibleRepositories();
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: AppSpacing.xs,
-        children: _recommendedSources
-            .map(
-              (e) => Material(
-                borderRadius: BorderRadius.circular(8),
-                clipBehavior: Clip.hardEdge,
-                child: InkWell(
-                  mouseCursor: SystemMouseCursors.click,
-                  hoverColor: Theme.of(context).colorScheme.primaryContainer,
-                  onTap: () async => await launchUrl(Uri.parse(e)),
-                  child: Container(
-                    width: double.infinity,
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16, horizontal: 12),
-                    decoration: BoxDecoration(
-                        // color: Theme.of(context).colorScheme.surfaceContainer,
-                        ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(e),
-                        Icon(
-                          Icons.open_in_new,
-                          color: Theme.of(context).colorScheme.onSurfaceVariant,
-                          size: 16,
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            )
-            .toList());
   }
 }
