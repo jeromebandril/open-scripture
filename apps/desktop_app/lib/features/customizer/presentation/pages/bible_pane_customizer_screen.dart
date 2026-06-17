1c82a835d87fbee2cbafe0457b24b342fe4eee18
jerome <jerome.bandril@gmail.com>
2026-06-17 23:44:33 +0200

    refactor: use my tab bar container in ui

--- apps/desktop_app/lib/features/customizer/presentation/pages/bible_pane_customizer_screen.dart
+++ apps/desktop_app/lib/features/customizer/presentation/pages/bible_pane_customizer_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:open_scripture/features/customizer/presentation/pages/bible_pane_general_customizer_screen.dart';
 import 'package:open_scripture/features/customizer/presentation/pages/bible_view_list_customizer_screen.dart';
 import 'package:open_scripture/features/customizer/presentation/pages/bible_view_presentation_customizer_screen.dart';
-import 'package:open_scripture/features/settings_window/presentation/widgets/setting_subpage_navigator.dart';
+import 'package:open_scripture/shared/widgets/ui/b_container_tab_bar.dart';
 
 class BiblePaneCustomizerScreen extends StatefulWidget {
   const BiblePaneCustomizerScreen({super.key});
@@ -13,7 +13,6 @@ class BiblePaneCustomizerScreen extends StatefulWidget {
 }
 
 class _BiblePaneCustomizerScreenState extends State<BiblePaneCustomizerScreen> {
-  int _index = 0;
   late final List<Widget> _pages;
 
   @override
@@ -30,45 +29,9 @@ class _BiblePaneCustomizerScreenState extends State<BiblePaneCustomizerScreen> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.fromLTRB(42, 0, 42, 42),
-      child: Column(
-        spacing: 8,
-        children: [
-          SettingSubpageNavigator(
-            selectedId: _index,
-            data: [
-              SettingSubpageNavigatorData(
-                id: 0,
-                onSelect: (id) => setState(() {
-                  _index = id;
-                }),
-                icon: Icon(Icons.color_lens_rounded),
-                title: 'General',
-              ),
-              SettingSubpageNavigatorData(
-                id: 1,
-                onSelect: (id) => setState(() {
-                  _index = id;
-                }),
-                icon: const Icon(Icons.list),
-                title: 'List view',
-              ),
-              SettingSubpageNavigatorData(
-                id: 2,
-                onSelect: (id) => setState(() {
-                  _index = id;
-                }),
-                icon: const Icon(Icons.screenshot_monitor_rounded),
-                title: 'Presentation view',
-              ),
-            ],
-          ),
-          Expanded(
-            child: IndexedStack(
-              index: _index,
-              children: _pages,
-            ),
-          ),
-        ],
+      child: BContainerTabBar(
+        tabs: ['General', 'List View', 'Presentation View'],
+        views: _pages,
       ),
     );
   }
