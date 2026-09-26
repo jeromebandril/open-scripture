cdc5d7b4aa077dc94dca4aafec3d133b9d386e5f
Jerome Bandril <jerome.bandril@gmail.com>
2026-09-26 16:26:13 +0200

    fix: suppress global shortcut when app windows are opened

--- apps/desktop_app/lib/app/extensions/build_context_extensions.dart
+++ apps/desktop_app/lib/app/extensions/build_context_extensions.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../features/shortcuts/presentation/widgets/shortcuts_scope_suppressed.dart';
 import '../../shared/design_system/design_system.dart';
+import '../state/fullscreen_cubit.dart';
 import '../widgets/app_window.dart';
 import '../widgets/titlebar.dart';
 
@@ -8,29 +11,36 @@ extension AppBuildContextExtensions on BuildContext {
   Future<T?> pushWindow<T>({
     required WidgetBuilder builder,
   }) {
+    final isFullscreen = read<FullscreenCubit>().state;
+
     return Navigator.of(this, rootNavigator: true).push(
       DialogRoute<T>(
         context: this,
         barrierDismissible: false,
         barrierColor: const Color(0x99000000),
         builder: (context) {
-          return Column(
-            children: [
-              const Titlebar(showMenuBar: false),
-              Expanded(
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.all(AppSpacing.xl2),
-                    child: Material(
-                      elevation: 24,
-                      color: Theme.of(context).colorScheme.surface,
-                      borderRadius: BorderRadius.circular(AppRadius.lg),
-                      child: builder(context),
+          return ShortcutsScopeSuppressed(
+            child: FocusScope(
+              autofocus: true,
+              child: Column(
+                children: [
+                  if (!isFullscreen) const Titlebar(showMenuBar: false),
+                  Expanded(
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(AppSpacing.xl2),
+                        child: Material(
+                          elevation: 24,
+                          color: Theme.of(context).colorScheme.surface,
+                          borderRadius: BorderRadius.circular(AppRadius.lg),
+                          child: builder(context),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           );
         },
       ),
